de45a38a38389c9534f8d003ba3d788397d42431
ithaksinterlance <it.haksinterlance@gmail.com>
2021-03-16 21:47:57 +0530

    commit

--- src/components/main.js
+++ src/components/main.js
@@ -5,7 +5,6 @@ import Solite from "./Solite";
 import Varta from "./Varta";
 import Acdelco from "./Acdelco";
 import Amaron from "./Amaron";
-import Form from "./form";
 import Brands from "./brands";
 import Services from "./services";
 import Sell from "./sell";
@@ -18,7 +17,6 @@ const Main = () => (
     <Route exact path="/varta" component={Varta} />
     <Route exact path="/acdelco" component={Acdelco} />
     <Route exact path="/amaron" component={Amaron} />
-    <Route exact path="/form" component={Form} />
     <Route exact path="/brands" component={Brands} />
     <Route exact path="/services" component={Services} />
     <Route exact path="/sell" component={Sell} />
