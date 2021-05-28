c967d91ce50112987f3d39dc2ac3770511789d2e
ithaksinterlance <it.haksinterlance@gmail.com>
2021-05-28 15:23:58 +0530

    commit

--- src/components/sitemap-generator.js
+++ src/components/sitemap-generator.js
@@ -1,10 +1,9 @@
+import routes from "./main";
 require('@babel/register');
-const router = require('./main').default;
+const router = require(routes).default;
 const Sitemap = require('react-router-sitemap').default;
 (
     new Sitemap(router)
         .build('https://autobatteries.hakstime.com')
         .save('./sitemap.xml')
 );
-
-
