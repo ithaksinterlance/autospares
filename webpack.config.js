56e0bc1744673c9a631e4990a59d23e99feb5c6e
ithaksinterlance <it.haksinterlance@gmail.com>
2021-06-30 12:39:49 +0530

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath:"/autospares"
   },
   target: 'node',
     externals: [nodeExternals()],
