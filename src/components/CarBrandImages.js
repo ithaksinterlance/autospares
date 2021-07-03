368d78789669334b20bd33d49a34b9fc258d99d3
ithaksinterlance <it.haksinterlance@gmail.com>
2021-07-03 12:17:56 +0530

    commit

--- src/components/CarBrandImages.js
+++ src/components/CarBrandImages.js
@@ -4,7 +4,7 @@ export default class CarBrandImages extends Component {
   render() {
     return this.props.images.map((image) => {
       return (
-        <img src={image.logo} className="img-fluid brandImg shadow-sm" alt={image.name} />
+        <img src={image.logo} className="img-fluid brandImg shadow-sm" alt="car battery dubai" />
       );
     });
   }
