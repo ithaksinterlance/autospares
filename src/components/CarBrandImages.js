a24a15e9c3c3f36acc493f33e440ce52de21b690
ithaksinterlance <it.haksinterlance@gmail.com>
2021-07-05 08:36:30 +0530

    commit

--- src/components/CarBrandImages.js
+++ src/components/CarBrandImages.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/opacity.css";
 
 export default class CarBrandImages extends Component {
   render() {
     return this.props.images.map((image) => {
       return (
-        <img src={image.logo} className="img-fluid brandImg shadow-sm" alt={image.alt} />
+        <LazyLoadImage
+          alt={image.alt}
+          effect="opacity"
+          className="img-fluid brandImg shadow-sm"
+          src={image.logo}
+        />
       );
     });
   }
