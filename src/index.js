0d71e27122269f1efec47f377d6980ab4885f146
ithaksinterlance <it.haksinterlance@gmail.com>
2021-07-07 23:17:01 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <Router><App /></Router>,
+  <HashRouter><App /></HashRouter>,
   document.getElementById("root")
 );
 
