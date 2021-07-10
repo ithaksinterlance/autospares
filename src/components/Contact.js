c25aafd77bd4629eb51fb849fd69f10ccdaa3e4c
ithaksinterlance <it.haksinterlance@gmail.com>
2021-07-10 11:29:04 +0530

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Footer from "./Footer.js";
 import Navbar from "./Navbar.js";
 import Swal from "sweetalert2";
@@ -12,29 +12,23 @@ export default class Contact extends Component {
 
     if (carbattery === "") {
       submitted = false;
-      document.getElementById("carbatteryerror").innerHTML =
-        "** Please fill the Battery Model";
-      //return false;
+      document.getElementById("carbatteryerror").innerHTML = "** Please fill the Battery Model";
+      // return false;
     } else if (area === "") {
       submitted = false;
-      document.getElementById("areaerror").innerHTML =
-        "** Please fill the Area";
-      //return false;
+      document.getElementById("areaerror").innerHTML = "** Please fill the Area";
+      // return false;
     } else if (contactno === "") {
       submitted = false;
-      document.getElementById("contactnoerror").innerHTML =
-        "** Please fill the Contact";
-      //return false;
+      document.getElementById("contactnoerror").innerHTML = "** Please fill the Contact";
+      // return false;
     } else if (isNaN(contactno)) {
       submitted = false;
-      document.getElementById("contactnoerror").innerHTML =
-        "** Check Your contact no";
-      //return false;
+      document.getElementById("contactnoerror").innerHTML = "** Check Your contact no";
+      // return false;
     } else {
       if (submitted) {
-        Swal.fire("Form submitted. We will contact you shortly ;)").then(() =>
-          document.getElementById("myForm").onreset(window.location.reload())
-        );
+        Swal.fire("Form submitted. We will contact you shortly ;)").then(() => document.getElementById("myForm").onreset(window.location.reload()));
         return submitted;
       }
     }
@@ -42,96 +36,64 @@ export default class Contact extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar/>
         <div className="container pt-5 text-center">
           <div className="row justify-content-center">
-              <h1 className="heading">We have made it easier :)! Fill the following form. We will be contact you through whatsapp</h1>
-            </div>
+            <h1 className="heading">We have made it easier :)! Fill the following form. We will be contact you through whatsapp</h1>
           </div>
-        <div class="container-fluid pt-5 form-bg">
+        </div>
+
+        <div class="container-fluid py-5 form-bg">
           <div class="row justify-content-center">
+            <div className="col-md-6">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d462423.69677338307!2d55.0367571!3d25.1125178!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1625891911862!5m2!1sen!2sin" width="600" height="450" style={{border:"0"}} title="car battery dubai" allowfullscreen="" loading="lazy"></iframe>
+            </div>
             <div class="col-md-4 col-xs-12 col-sm-10  ">
-              <iframe
-                title="car battery replacement uae"
-                name="hidden_iframe"
-                id="hidden_iframe"
-                style={{ display: "none" }}
-              ></iframe>
-              <form
-                id="myForm"
-                method="POST"
-                action="https://docs.google.com/forms/d/e/1FAIpQLSd6fL_M5hLUjjBLJyMFWUijW2qNrGwQ0RxMVhpmTOEEqj3Jxg/formResponse"
-                onSubmit={this.submituserInquiryForm}
+            <h1 className="paragraph">Order your battery now!</h1>
+            <hr/>
+              <iframe title="car battery replacement uae" name="hidden_iframe" id="hidden_iframe"
+                style={
+                  {display: "none"}
+              }></iframe>
+              <form id="myForm" method="POST" action="https://docs.google.com/forms/d/e/1FAIpQLSd6fL_M5hLUjjBLJyMFWUijW2qNrGwQ0RxMVhpmTOEEqj3Jxg/formResponse"
+                onSubmit={
+                  this.submituserInquiryForm
+                }
                 class="py-4 px-4"
-                target="hidden_iframe"
-              >
+                target="hidden_iframe">
 
                 <div class="form-group">
                   <small id="CarBatteryModel" class="form-text">
-                    <i class="fas fa-car-battery"></i> Vehicle Details
+                    <i class="fas fa-car-battery"></i>
+                    Vehicle Details
                   </small>
-                  <input
-                    type="text"
-                    name="entry.1911907904"
-                    id="carbattery"
-                    class="form-control "
-                    aria-describedby="CarBatteryModel"
-                    placeholder=" Car Model Year"
-                    autoComplete="off"
-                  />
+                  <input type="text" name="entry.1911907904" id="carbattery" class="form-control " aria-describedby="CarBatteryModel" placeholder=" Car Model Year" autoComplete="off"/>
                   <div>
-                    <span
-                      id="carbatteryerror"
-                      className="text-danger font-weight-bold"
-                    ></span>
+                    <span id="carbatteryerror" className="text-danger font-weight-bold"></span>
                   </div>
                 </div>
                 <div class="form-group">
                   <small id="AreaName" class="form-text">
-                    <i class="fa fa-map-marker" aria-hidden="true"></i> Delivery Location
+                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                    Delivery Location
                   </small>
-                  <input
-                    type="text"
-                    name="entry.1153362739"
-                    id="area"
-                    class="form-control"
-                    aria-describedby="AreaName"
-                    placeholder=" Area Name, Emirate"
-                    autoComplete="off"
-                  />
+                  <input type="text" name="entry.1153362739" id="area" class="form-control" aria-describedby="AreaName" placeholder=" Area Name, Emirate" autoComplete="off"/>
                   <div>
-                    <span
-                      id="areaerror"
-                      className="text-danger font-weight-bold"
-                    ></span>
+                    <span id="areaerror" className="text-danger font-weight-bold"></span>
                   </div>
                 </div>
                 <div class="form-group">
                   <small id="ContactNo" class="form-text">
-                    <i class="fa fa-phone" aria-hidden="true"></i> Contact
+                    <i class="fa fa-phone" aria-hidden="true"></i>
+                    Contact
                   </small>
-                  <input
-                    type="text"
-                    name="entry.153243795"
-                    id="contactno"
-                    class="form-control "
-                    aria-describedby="ContactNo"
-                    placeholder="(Country code)(No.)"
-                    autoComplete="off"
-                  />
+                  <input type="text" name="entry.153243795" id="contactno" class="form-control " aria-describedby="ContactNo" placeholder="(Country code)(No.)" autoComplete="off"/>
                   <div>
-                    <span
-                      id="contactnoerror"
-                      className="text-danger font-weight-bold"
-                    ></span>
+                    <span id="contactnoerror" className="text-danger font-weight-bold"></span>
                   </div>
                 </div>
                 <input type="hidden" id="ref" name="entry.376651987" value="AJB"/>
-                <button
-                  type="submit"
-                  class="btn btn-danger btn-lg btn-block pb-3 form-btn"
-                  target="_self"
-                >
+                <button type="submit" class="btn btn-danger btn-lg btn-block pb-3 form-btn" target="_self">
                   <i class="fab fa-whatsapp"></i>
                   &nbsp;&nbsp;SEND
                 </button>
@@ -142,7 +104,7 @@ export default class Contact extends Component {
         </div>
 
         <div className="text-center">
-        <Footer />
+          <Footer/>
         </div>
 
       </div>
