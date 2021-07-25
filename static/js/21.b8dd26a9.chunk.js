68a40deb7f8f11304216ccb5f008dcf7fbc6db41
ithaksinterlance <it.haksinterlance@gmail.com>
2021-07-25 20:59:37 +0530

    Updates

--- static/js/21.b8dd26a9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonphaksinterlance=this.webpackJsonphaksinterlance||[]).push([[21],{136:function(n,t,e){"use strict";e.r(t);t.default=function(n){n&&n instanceof Function&&e.e(22).then(e.bind(null,125)).then((function(t){var e=t.getCLS,i=t.getFID,c=t.getFCP,s=t.getLCP,a=t.getTTFB;e(n),i(n),c(n),s(n),a(n)}))}}}]);
-//# sourceMappingURL=21.b8dd26a9.chunk.js.map
